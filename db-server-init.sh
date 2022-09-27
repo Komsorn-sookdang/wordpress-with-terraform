3f3d3ec3191a867784dd05254f12fbd4a818d84f
มึน <komsornzaza@gmail.com>
2022-09-28 03:55:57 +0700

    make db-server can access form anywhere

--- db-server-init.sh
+++ db-server-init.sh
@@ -12,8 +12,12 @@ apt install mariadb-server mariadb-client -y
 echo 'create && set up database' >> /home/ubuntu/terraform-log.txt
 # sudo mysql -u root --password=  
 mysql -u root -e "CREATE DATABASE wordpress_db"
-mysql -u root -e "CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'password'"
-mysql -u root -e "GRANT ALL ON wordpress_db.* TO 'wp_user'@'localhost' IDENTIFIED BY 'password'"
+mysql -u root -e "CREATE USER 'wp_user'@'%' IDENTIFIED BY 'password'"
+mysql -u root -e "GRANT ALL ON wordpress_db.* TO 'wp_user'@'%' IDENTIFIED BY 'password'"
 mysql -u root -e "FLUSH PRIVILEGES"
+
+echo 'set bind-address to anywhere' >> /home/ubuntu/terraform-log.txt
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+systemctl restart mysql
 
 echo 'complete' >> /home/ubuntu/terraform-log.txt
